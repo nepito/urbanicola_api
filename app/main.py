143a23ea641ada75c622226784f78f018dd0145b
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-11-06 20:21:42 -0800

    🚧 Add query for gastos

--- app/main.py
+++ app/main.py
@@ -1,15 +1,29 @@
 from fastapi import FastAPI
 import json
+import pandas as pd
 
+expenses = pd.read_csv("./data/gastos.csv")
+f = open("./data/players.json")
+players = json.load(f)
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
+@app.get("/v1")
+def dummy_request():
     return {"Hello": "World"}
 
 
 @app.get("/make_add")
 def make_add():
     return {"add": 4 + 6}
+
+
+@app.get("/v1/players")
+def get_player():
+    return players["response"]
+
+
+@app.get("/v1/gastos")
+def get_expenses():
+    return expenses.to_json(orient="records")
