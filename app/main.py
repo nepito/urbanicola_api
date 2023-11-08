4cd4a99e255fa1cad6241dfb54b38f3f546bf62e
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-11-07 22:24:44 -0800

    🚧 Add post query

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 import json
 import pandas as pd
+from pydantic import BaseModel
 
+
+class Item(BaseModel):
+    a: list[int]
+    b: list[int]
+    c: list[bool]
+
+item = Item(**{"a":[0], "b":[0], "c":[False]})
 expenses = pd.read_csv("./data/gastos.csv")
 f = open("./data/players.json")
 players = json.load(f)
@@ -27,3 +35,12 @@ def get_player():
 @app.get("/v1/gastos")
 def get_expenses():
     return expenses.to_json(orient="records")
+
+
+@app.post("/v1/item")
+def create_item(item: Item):
+    test_data = pd.read_csv("./data/test_data.csv")
+    df_row = pd.DataFrame(item.dict())
+    new_df = pd.concat([test_data, df_row])
+    new_df.to_csv("./data/test_data.csv", index=False)
+    return item.dict()
