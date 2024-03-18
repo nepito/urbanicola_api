4f1579e43c3d4f3897c9b6dbb02ca20ab438ff17
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-03-18 13:45:18 -0700

    ✅🗃️ Run test

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
 """API for Urbanicola"""
 
 __version__ = "0.1.0"
+from .main import *  # noqa
--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -1,9 +1,25 @@
-import urbanicola_api as dt
+from app import app
+from fastapi.testclient import TestClient
+
+client = TestClient(app)
+
+gasto = {
+    "date": ["2024-03-18"],
+    "mount": [0],
+    "type": ["string"],
+    "concept": ["string"],
+    "subtype": ["string"],
+    "area": ["string"],
+    "how_many": [0],
+    "provider": ["string"],
+    "factura": [True],
+    "payment_type": ["string"],
+    "bank_count": ["string"],
+    "description": ["string"],
+}
 
 
-def test_add_offset():
-    augend = 1
-    addend = 2
-    expected = augend + addend
-    obtained = dt.add_offset(augend, addend)
-    assert expected == obtained
+def test_read_main():
+    response = client.post("/v1/spent/", json=gasto)
+    assert response.status_code == 200
+    assert response.json()["description"] == ["string"]
